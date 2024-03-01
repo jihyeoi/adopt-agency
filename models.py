3e827c2a8d2dbb187fff7dba9e41800ba2aa3ff8
JiHye Yoon <yoonjihyeoi@gmail.com>
2024-03-01 12:07:24 -0800

    finished step 1

---
+++ models.py
@@ -0,0 +1,48 @@
+"""Models for adopt app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class Pet(db.Model):
+
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True)
+
+    name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    species = db.Column(
+        db.String(50),
+        nullable=False)
+
+    photo_url = db.Column(
+        db.Text,
+        nullable=False,
+        default='')
+
+    age = db.Column(
+        db.Text,
+        nullable=False)
+
+    notes = db.Column(
+        db.Text)
+
+    available = db.Column(
+        db.Boolean,
+        default=True)
